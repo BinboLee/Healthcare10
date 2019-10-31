cbb4202e6a7c6ccc5a9911f32bd13ef9e1fb3415
BinboLee <li_tuoyang@163.com>
2019-10-31 12:16:04 +0800

    test,.

---
+++ app/src/main/java/com/example/healthcare10/Item1.java
@@ -0,0 +1,20 @@
+package com.example.healthcare10;
+
+public class Item1 {
+    private String memo;
+
+    public Item1() {
+    }
+
+    public Item1(String memo) {
+        this.memo = memo;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+}
